bbf47ff9e2c8c39c8fe295c9130442de8d8906ce
flacial <ruthemiumwithdiamond@protonmail.com>
2021-09-08 18:34:14 +0400

    python/port-scanner: Refactor to use a class and work with cli arguments.

--- networking/port-scanner.py
+++ networking/port-scanner.py
@@ -1,58 +1,78 @@
 import socket
 import re
+import sys
 
 # socket.AF_INET = Internet Address Family ipv6
 # socket.SOCK_STREAM = Socket type TCP
 # Port max = 65535
-def portScanner() -> None:
-    ipv4Re = "([\d]+[.][\d]+[.][\d]+[.][\d]+)"
-    portRe = "([\d]+[/][\d]+)"
-    openPortsCount = 0
-
-    print(
-        """
-Select Address Family:
-
-[1] ipv4
-[2] ipv6
-""")
+class PortScanner:
+    def __init__(self, addressFamily, host, port) -> None:
+        self.host = host
+        self.portRange = port
+        self.addressFamily = addressFamily
+        self.portScanner()
     
-    addressFamily = input("Choose an option number: ")
+    def createSocket(self, port) -> None:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, port))
+        s.close()
 
-    if addressFamily == "1":
-        host = input("\n[?] Host ip: ")
-        validateIp = re.match(ipv4Re, host)
+    def findOpenPorts(self, rangeStart: int, rangeEnd: int) -> None:
+        openPortsCount: int = 0
 
-        if hasattr(validateIp, "group"):
-            if validateIp.group() != host:
+        for port in range(int(rangeStart), int(rangeEnd) + 1):
+            try:
+                self.createSocket(port)
+                openPortsCount += 1
+                print(f"{port}   Open")
+            except:
+                pass
+        if openPortsCount == 0:
+            print("\n[!] No open ports found")
+            return
+        print(f"\n[#] {openPortsCount} open port{'s' if openPortsCount > 1 else ''} were found")
+
+    def getPortRange(self):
+        portRe: str = "([\d]+[/][\d]+)"
+        # portRange: str = input("[?] Port range to scan (e.g. 20/300): ")
+
+        validatePort: object = re.match(portRe, self.portRange)
+        portMatch: bool = hasattr(validatePort, "group")
+
+        if portMatch:
+            if validatePort.group() != self.portRange:
+                print("\n[!] Type a valid port range")
+                return
+            rangeStart, rangeEnd = self.portRange.split("/")
+            return rangeStart, rangeEnd
+        print("\n[!] Type a valid port range")
+
+    def ipv4Scan(self):
+        ipv4Re: str = "([\d]+[.][\d]+[.][\d]+[.][\d]+)"
+
+        validateIp: object = re.match(ipv4Re, self.host)
+        ipMatch: bool = hasattr(validateIp, "group")
+
+        if ipMatch:
+            if validateIp.group() != self.host:
                 print("\n[!] Type a valid ip address")
                 return
-
-            portRange = input("[?] Port range to scan (e.g. 20/300): ")
-            validatePort = re.match(portRe, portRange)
-
-            if hasattr(validatePort, "group"):
-                if validatePort.group() != portRange:
-                    print("\n[!] Type a valid port range")
-                    return
-                rangeStart, rangeEnd = portRange.split("/")
-
-                for port in range(int(rangeStart), int(rangeEnd) + 1):
-                    try:
-                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        s.connect((host, port))
-                        s.close()
-                        openPortsCount += 1
-                        print(f"{port}   Open")
-                    except:
-                        pass
-
-                if openPortsCount == 0:
-                    print("\n[!] No open ports found")
-                return
-            print("\n[!] Type a valid port range")
+            rangeStart, rangeEnd = self.getPortRange()
+            self.findOpenPorts(int(rangeStart), int(rangeEnd))
             return
         print("\n[!] Type a valid ip address")
 
+    def portScanner(self) -> None:
+        if self.addressFamily == "ipv4":
+            self.ipv4Scan()
+            return
 
-portScanner()
+if __name__ == "__main__":
+    opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+
+    if all(opt in opts for opt in ['-af', '-h', '-pr']):
+        addressFamily, ip, port = args
+        PortScanner(addressFamily, ip, port)
+    else:
+        raise SystemExit(f"Usage: {sys.argv[0]} -af <address family (ipv4 or ipv6)> -h <ip address> -pr <port range (e.g. 300/100)>")
