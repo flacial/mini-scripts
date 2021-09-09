5b04da35e07cbc8bf6f99d7c01dee658138b8e4c
flacial <ruthemiumwithdiamond@protonmail.com>
2021-09-09 22:37:20 +0400

    python/port-scanner: handle Windows exception

--- networking/port-scanner.py
+++ networking/port-scanner.py
@@ -8,6 +8,7 @@ class PortScanner:
         self.host = host
         self.portRange = port
         self.addressFamily = addressFamily
+        self.openPorts: list = []
         self.portScanner()
 
     def createSocket(self, port) -> None:
@@ -15,28 +16,37 @@ class PortScanner:
         s.connect((self.host, port))
         s.close()
 
+    def scanPort(self, port):
+        try:
+            self.createSocket(port)
+            self.openPorts.append(port)
+            print(f"{port}    open")
+        except (OSError, ConnectionRefusedError) as e:
+            if e.errno == 113:
+                print("[!] No route to host. Make sure the host is reachable")
+                return
+            if e.errno == 10061:
+                print(
+                    "[!] No connection could be made because the target machine actively refused it. Check your firewall settings."
+                )
+                return
+
     def findOpenPorts(self, rangeStart: int, rangeEnd: int) -> None:
-        openPortsCount: int = 0
         portRange = range(int(rangeStart), int(rangeEnd) + 1)
-        
+
         if portRange[-1] > 65535:
-            return print('[!] Port rangeEnd is greater than 65535')
-        
-        print('Attempting to Scan...\n')
+            return print("[!] Port rangeEnd is greater than 65535")
+
+        print("Attempting to Scan...\n")
         for port in portRange:
-            try:
-                self.createSocket(port)
-                openPortsCount += 1
-                print(f"{port}   Open")
-            except OSError as e:
-                if e.errno == 113:
-                    print('[!] No route to host. Make sure the host is reachable')
-                    return
-        if openPortsCount == 0:
+            self.scanPort(port)
+
+        if len(self.openPorts) == 0:
             print("\n[!] No open ports found")
             return
+
         print(
-            f"\n[#] {openPortsCount} open port{'s' if openPortsCount > 1 else ''} were found"
+            f"\n[#] {len(self.openPorts)} open port{'s' if len(self.openPorts) > 1 else ''} were found"
         )
 
     def getPortRange(self):
@@ -71,7 +81,8 @@ class PortScanner:
     def portScanner(self) -> None:
         if self.addressFamily == "ipv4":
             return self.ipv4Scan()
-        print('[!] Address family must be ipv4. ipv6 is not supported yet')
+        print("[!] Address family must be ipv4. ipv6 is not supported yet")
+
 
 if __name__ == "__main__":
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
